63e79f8cec6b5482fc7682f1e0647f396723d229
Jason <bjason@gmail.com>
2018-02-05 22:41:15 -0800

    broken on mouse over

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log('main.js');
 
-for(var i = 0; i < 1000; i++ ) {
-    var $el = '<img src="broken.png"/>'
+for(var i = 0; i < 10000; i++ ) {
+    var $el = $('<img src="broken.png"/>'); 
     $(document.body).append($el);
-
+    $el.on('mouseover', function() {
+      $(this).css('opacity', 0.1);
+    });
 }
